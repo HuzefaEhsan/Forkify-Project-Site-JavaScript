97a807ff2e8ea3aeffc5d28f188aeb096d96c808
Huzefa Ehsan <huzaifaehsan207@gmail.com>
2022-10-15 20:55:33 +0500

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = `https://forkify-api.herokuapp.com/api/v2/recipes/`;
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '03653734-49c0-49bf-b683-41f8942dba09';
+export const MODAL_CLOSE_SEC = 2.5;
